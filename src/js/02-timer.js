d8d6514a959286f0426731f042107eaf30c80998
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2022-12-04 20:10:07 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -77,10 +77,10 @@ const renderTimer = ({ days, hours, minutes, seconds }) => {
 };
 
 startBtn.addEventListener('click', evt => {
-  flatpickrRef.element.disabled = true;
+  //flatpickrRef.element.disabled = true;
   //console.log(flatpickrRef.element.disabled);
-  inputDate.setAttribute('disabled', 'disabled');
-
+  //inputDate.setAttribute('disabled', 'disabled');
+  inputDate.disabled = !inputDate.disabled;
   evt.target.disabled = true;
   timerId = setInterval(setValues, 1000);
 });
